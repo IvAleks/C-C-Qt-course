5bdc3db74d37f6ade300a51a1e8f761c570e6c50
IvAlekks <ivlelik2012@gmail.com>
2020-03-23 21:13:35 +0300

    dz2.c

---
+++ dz2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+  int main () 
+  {
+  char simb=127;
+  printf("simb:%d\n",simb);
+  printf("simb:%c\n",simb);
+  simb++;
+  printf("simb:%c\n",simb);
+  printf("simb:%d\n",simb);
+  //rintf("simb:%d\n",c);
+  // int c;
+  // c= (int)simb;
+  // printf("simb:%d\n",c);
+  
+  return 0;
+}
