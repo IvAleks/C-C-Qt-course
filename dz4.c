7ceef641994fef25feeb2d2ccddcaa7deba3e641
IvAleks <ivlelik2012@gmail.com>
2020-05-07 22:37:44 +0300

    dz4.c

--- dz4.c
+++ dz4.c
@@ -1,44 +1,17 @@
+// Пример рекурсии для расчета суммы цыфр дня рождения 
 #include <stdio.h>  
 int main()
-{
-printf(" Введите число от 0 до 9\n");
-int num;
-scanf("%d", &num); // ввод числа
-int a=num;
-	switch (a) {
-  	case 1:
-    		printf(" one \n");
-		break;
-	case 2:
-		printf(" two \n");
-		break;
-	case 3:
-		printf(" three \n");
-		break;	
-	case 4:
-		printf(" four \n");
-		break;	
-	case 5:
-		printf(" five \n");
-		break;
-	case 6:	
-		printf(" six \n");
-		break;
-	case 7:
-		printf(" seven \n");
-		break;	
-	case 8:
-		printf(" eight \n");
-		break;
-	case 9:
-		printf(" nine \n");
-		break;
-	case 0:
-		printf(" zero \n");
-		break;
-	default:
-		printf( "Введите число от 0 до 9  \n" );
-}
+{   
+int number, summ=0;
+printf(" Введите дату рождения без разделительных символов\n");
+scanf("%d", &number); // ввод
+	int func(int num)
+	{
+	if (num<10) return num;
+	else return num%10+func(num/10);
+    }
+summ=func(number);
+printf("сумма чисел = %d \n", summ);
 return 0;
 }
 
